2054a508feabcc02c06b2c931728014e78e5ff8a
lemon_wonder <lemon_wonder@163.com>
2013-12-09 23:07:24 +0800

    add base head class

---
+++ server/include/CLHead.h
@@ -0,0 +1,15 @@
+#ifndef CLHEAD_H
+#define CLHEAD_H
+
+class CLHead
+{
+public:
+	explicit CLHead()
+	{}
+	virtual ~CLHead()
+	{}
+	virtual char* serialize(){}
+	virtual void deserialize(char *buffer){}
+};
+
+#endif
